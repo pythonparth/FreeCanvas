5461367a7153d5e40559243968c4935aec5d5046
pythonparth <92641821+pythonparth@users.noreply.github.com>
2023-05-09 19:49:31 +0530

    1st commit

---
+++ FreeCanvas/README.md
@@ -0,0 +1,40 @@
+## About Snap and Quick Editor  
+Snap and Quick editor is a fully functional photo editor web application.
+Here you can use multiple filters and edit tools to edit your photo.
+Its provided with a reset and save button . 
+the Key feature is you can save the edited image in the form of jpg directly to your storage.
+
+## Gallery
+|Home Screen|Editing Screen|
+|-------|-------|
+|<img src="iamge editor/ss- 0.png" width="500" height="300" />|<img src="/iamge editor/ss -1.png" width="500" height="300" />
+
+
+
+
+|Features Screen|Edited Screen|
+|-------|-------|
+|<img src="iamge editor/ss-2.png" width="500" height="300" />|<img src="iamge editor/ss-3.png" width="500" height="300" />|
+
+
+## Build The Code
+1. Clone the repository https://github.com/pratham-ez/Snap-and-Quick-Photo-Editor-.git
+
+
+## Contribution Guidelines
+1. All issues should be raised via GitHub issue tracker and should follow the issue template
+2. All changes should be proposed via Pull Requests and should follow the pull request template
+3. Commit messages should be appropriate and must reflect/describe the changes made
+4. One commit should have only one change although it may involve changes in multiple files
+
+## How To Contribute
+1. Fork the project
+2. Make your changes and push the changes to your forked repository
+3. Raise a pull-request dev branch and ensure the pull request follows the template.
+4. At the moment we are adding all the new features to the dev branch, and pushing only fully features to our master branch.
+
+## Documentation
+- Languages: Javascript , HTML , CSS          
+## Maintainer
+- [Pratham Khodwe](https://github.com/pratham-ez)
+
---
+++ FreeCanvas/iamge editor/script.js
@@ -0,0 +1,135 @@
+const fileInput = document.querySelector(".file-input"),
+  filterOptions = document.querySelectorAll(".filter button");
+filterName = document.querySelector(".filter-info .name");
+filterValue = document.querySelector(".filter-info .value");
+filterSlider = document.querySelector(".slider input");
+rotateOptions = document.querySelectorAll(".rotate button");
+previewImg = document.querySelector(".preview-img img");
+resetFilterBtn = document.querySelector(".reset-filter");
+saveImageBtn = document.querySelector(".save-image");
+chooseImgBtn = document.querySelector(".choose-image");
+
+let brightness = 100,
+  saturation = 100,
+  inversion = 0,
+  grayscale = 0;
+
+let rotate = 0,
+  flipHorizontal = 1,
+  flipVertical = 1;
+
+const applyFilter = () => {
+  previewImg.style.transform = `rotate(${rotate}deg) scale(${flipHorizontal} , ${flipVertical})`;
+
+  previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}) grayscale(${grayscale})`;
+};
+
+const loadImage = () => {
+  let file = fileInput.files[0];
+  if (!file) return;
+  previewImg.src = URL.createObjectURL(file);
+
+  previewImg.addEventListener("load", () => {
+    resetFilterBtn.click();
+    document.querySelector(".container").classList.remove("disable");
+  });
+};
+
+filterOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    document.querySelector(".filter .active").classList.remove("active");
+    option.classList.add("active");
+    filterName.innerText = option.innerText;
+
+    if (option.id === "Brightness") {
+      filterSlider.max = "200";
+      filterSlider.value = brightness;
+      filterValue.innerText = `${brightness}%`;
+    } else if (option.id === "Saturation") {
+      filterSlider.max = "200";
+      filterSlider.value = saturation;
+      filterValue.innerText = `${saturation}%`;
+    } else if (option.id === "Inversion") {
+      filterSlider.max = "100";
+      filterSlider.value = inversion;
+      filterValue.innerText = `${inversion}%`;
+    } else {
+      filterSlider.max = "100";
+      filterSlider.value = grayscale;
+      filterValue.innerText = `${grayscale}%`;
+    }
+  });
+});
+
+const updateFilter = () => {
+  filterValue.innerText = `${filterSlider.value}%`;
+  const selectedFilter = document.querySelector(".filter .active");
+
+  if (selectedFilter.id === "Brightness") {
+    brightness = filterSlider.value;
+  } else if (selectedFilter.id === "Saturation") {
+    saturation = filterSlider.value;
+  } else if (selectedFilter.id === "Inversion") {
+    inversion = filterSlider.value;
+  } else {
+    grayscale = filterSlider.value;
+  }
+
+  applyFilter();
+};
+
+rotateOptions.forEach((option) => {
+  option.addEventListener("click", () => {
+    if (option.id === "left") {
+      rotate -= 90;
+    } else if (option.id === "right") {
+      rotate += 90;
+    } else if (option.id === "horizontal") {
+      flipHorizontal = flipHorizontal === 1 ? -1 : 1;
+    } else {
+      flipVertical = flipVertical === 1 ? -1 : 1;
+    }
+    applyFilter();
+  });
+});
+
+const resetFilter = () => {
+  (brightness = 100), (saturation = 100), (inversion = 0), (grayscale = 0);
+
+  (rotate = 0), (flipHorizontal = 1), (flipVertical = 1);
+  filterOptions[0].click();
+  applyFilter();
+};
+
+const saveImage = () => {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = previewImg.naturalWidth;
+  canvas.height = previewImg.naturalHeight;
+
+  ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}) grayscale(${grayscale})`;
+
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+  if (rotate !== 0) {
+    ctx.rotate((rotate * Math.PI) / 180);
+  }
+
+  ctx.scale(flipHorizontal, flipVertical);
+  ctx.drawImage(
+    previewImg,
+    -canvas.width / 2,
+    -canvas.height / 2,
+    canvas.width,
+    canvas.height
+  );
+
+  const link = document.createElement("a");
+  link.download = "image.jpg";
+  link.href = canvas.toDataURL();
+  link.click();
+};
+fileInput.addEventListener("change", loadImage);
+filterSlider.addEventListener("input", updateFilter);
+resetFilterBtn.addEventListener("click", resetFilter);
+saveImageBtn.addEventListener("click", saveImage);
+chooseImgBtn.addEventListener("click", () => fileInput.click());
